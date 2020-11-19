ed579b2f37d8d9679cf394dc1bef1bfa07acf452
mlavan <40339489+mlavan303@users.noreply.github.com>
2020-11-19 21:17:55 +0100

    19.11.2020-21:17
    Removed ExampleMod.java
    Added ElectricalEngineeringMod.java

--- src/main/java/net/mlavan303/electricalengineeringmod/ElectricalEngineeringMod.java
+++ src/main/java/net/mlavan303/electricalengineeringmod/ElectricalEngineeringMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.mlavan303.electricalengineeringmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -18,14 +18,12 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("electrical_engineering_mod")
-public class ExampleMod
-{
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+public class ElectricalEngineeringMod {
 
-    public ExampleMod() {
+	private static final Logger LOGGER = LogManager.getLogger();
+
+    public ElectricalEngineeringMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
